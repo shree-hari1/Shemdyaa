9f812f08c6f02dab8b0cb7a9bf08fe3380d469ac
shree-hari1 <156194177+shree-hari1@users.noreply.github.com>
2024-01-24 17:16:45 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,69 @@
+class calc {
+float add(float x, float y) {
+return x + y;
+}
+
+float sub(float x, float y) {
+return x - y;
+}
+
+float mul(float x, float y) {
+return x * y;
+}
+
+float div(float x, float y) {
+return x / y;
+}
+
+public static void main(String[] args) {
+calc c = new calc();
+float x = 10;
+float y = 20;
+
+float add = c.add(x, y);
+System.out.println(add);
+
+float sub = c.sub(x, y);
+System.out.println(sub);
+
+float mul = c.mul(x, y);
+System.out.println(mul);
+
+float div = c.div(x, y);
+System.out.println(div);
+}
+}class calc {
+float add(float x, float y) {
+return x + y;
+}
+
+float sub(float x, float y) {
+return x - y;
+}
+
+float mul(float x, float y) {
+return x * y;
+}
+
+float div(float x, float y) {
+return x / y;
+}
+
+public static void main(String[] args) {
+calc c = new calc();
+float x = 10;
+float y = 20;
+
+float add = c.add(x, y);
+System.out.println(add);
+
+float sub = c.sub(x, y);
+System.out.println(sub);
+
+float mul = c.mul(x, y);
+System.out.println(mul);
+
+float div = c.div(x, y);
+System.out.println(div);
+}
+}
